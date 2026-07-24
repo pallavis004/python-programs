97e0b58823e72cd5246c481519b64d875237ab95
pallavis004 <pallavisnair004@gmail.com>
2026-07-24 10:06:21 +0530

    words sentence

---
+++ wordsentence.py
@@ -0,0 +1,14 @@
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+frequency = {}
+
+for word in words:
+    if word in frequency:
+        frequency[word] = frequency[word] + 1
+    else:
+        frequency[word] = 1
+
+for word in frequency:
+    print(word, "=", frequency[word])
